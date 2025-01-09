63cb2028bf7af4524cdeeac4b4e577126c23900a
ramazon07-cmd <ustau2453@gmail.com>
2025-01-09 21:29:47 +0500

    requirements changed

--- manage.py
+++ manage.py
@@ -1,22 +1,12 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
-import sys
+from django.core.management import execute_from_command_line
+from django.core.asgi import get_asgi_application  # Use ASGI for serverless environments
 
+if __name__ == "__main__":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+    # Vercel expects an `app` callable
+    application = get_asgi_application()
 
-
-if __name__ == '__main__':
-    main()
+    # This will allow the Vercel server to use this callable
+    execute_from_command_line()
