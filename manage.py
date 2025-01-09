26aeb14a4f6185b1779ce74b55454a62cd5ddf34
ramazon07-cmd <ustau2453@gmail.com>
2025-01-09 21:32:29 +0500

    requirements changed

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from django.core.management import execute_from_command_line
 from django.core.asgi import get_asgi_application  # Use ASGI for serverless environments
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
     # Vercel expects an `app` callable
     application = get_asgi_application()
