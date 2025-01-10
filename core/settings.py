a316839aa0fc3822187b92586b6a56ff9c756ced
ramazon07-cmd <ustau2453@gmail.com>
2025-01-10 19:05:39 +0500

    media added

--- core/settings.py
+++ core/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'staticfiles/media'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
